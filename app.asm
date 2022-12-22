be2528ea864c9cbb15c27fb5b8d0cbf9aed3308c
MohamedSalamaMouse <slamtm608@gmail.com>
2022-12-22 16:44:19 +0200

    edit caps.inc &app.asm

--- app.asm
+++ app.asm
@@ -1,4 +1,4 @@
-;include caps.inc 
+include caps.inc 
 ;include dups.inc
 include spaces.inc
 
@@ -6,7 +6,11 @@ include spaces.inc
 
 .DATA       
 
-text DB "time  !tIMe  in lIfE   ,What  is  ea    .sy to   do is not  worth      worth?it ",10,13,"$"
+text DB "time  !tIMe  in lIfE   ,What  is  ea    .sy to   do is not  worth      worth?it ",'$'
+HELLO_LEN equ  $ - text
+TIT DB 'Natural-Language-Processing ',10,13,'$'
+;PROCESSING1 DB 'beginning of the sentence is a capital letter and the rest of the sentence is lowercase: ',10,13,'$'  
+ 
 AddSpaceOutput DB ?
 ;RemoveSpaceOutput DB ? 
 
@@ -16,16 +20,24 @@ AddSpaceOutput DB ?
  
     MAIN PROC FAR
         .STARTUP
-        
-        ;call CAPITAL_SMALL
+        LEA DX,TIT
+        CALL PRINT
+        ;LEA DX,PROCESSING1
+       ;CALL PRINT
+        call CAPITAL_SMALL
         call REMOVE_SPACES
         call ADD_SPACES
-
-
+       
 
             
         .EXIT
        
     MAIN ENDP
+    ;------------Print function
+    PRINT PROC NEAR
+    MOV AH,09H
+    INT 21H
+    RET
+    PRINT ENDP
     
 END MAIN
